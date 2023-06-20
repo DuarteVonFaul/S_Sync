55a9b31a78017948b2a0e7c89b09cbc0e3f864c0
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-20 15:43:09 -0300

    CHORE[Atualizar Task por ID]:Aqui fiz o Endpoint de Atualização da Task recebendo o TaskUpdateRequest e um ID como parametro

--- S_Sync/src/main/java/com/dvf/project/S_Sync/api/controller/TaskController.java
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/api/controller/TaskController.java
@@ -1,7 +1,9 @@
 package com.dvf.project.S_Sync.api.controller;
 
+import com.dvf.project.S_Sync.api.dto.request.TaskUpdateRequest;
 import com.dvf.project.S_Sync.domain.model.Task;
 import com.dvf.project.S_Sync.domain.services.TaskService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +15,11 @@ import java.util.Optional;
 @RequestMapping("/tasks")
 public class TaskController {
     private TaskService taskService;
+    private ModelMapper modelMapper;
 
-    public TaskController(TaskService taskService){
+    public TaskController(TaskService taskService, ModelMapper modelMapper){
         this.taskService = taskService;
+        this.modelMapper = modelMapper;
     }
 
 
@@ -39,6 +43,16 @@ public class TaskController {
         Optional<Task> task = taskService.find_by_id(id);
         if(task != null){
             return new ResponseEntity<>(task.get(), HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update_task(@PathVariable Integer id, @RequestBody TaskUpdateRequest newTaks ){
+        Task task = modelMapper.map(newTaks, Task.class);
+        task = taskService.update_task_by_id(task, id);
+        if(task != null){
+            return new ResponseEntity<>(task, HttpStatus.ACCEPTED);
         }
         return ResponseEntity.notFound().build();
     }

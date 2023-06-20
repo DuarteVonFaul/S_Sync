e976127ab88c8b3306dee06de8fcaf5c5a48cc30
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-20 10:52:50 -0300

    Initialize[]:Inicializando o projeto

---
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/SSyncApplication.java
@@ -0,0 +1,13 @@
+package com.dvf.project.S_Sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSyncApplication.class, args);
+	}
+
+}
---
+++ S_Sync/src/test/java/com/dvf/project/S_Sync/SSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvf.project.S_Sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

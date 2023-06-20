f942612adfbcc5ebc0325af30def53dde68d741c
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-20 15:42:10 -0300

    CHORE[Atualizar Task por ID]:Agora que criamos o DTO Request vamos criar uma função especifica que trabalha encima desse DTO

--- S_Sync/src/main/java/com/dvf/project/S_Sync/domain/services/TaskService.java
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/domain/services/TaskService.java
@@ -1,14 +1,31 @@
 package com.dvf.project.S_Sync.domain.services;
 
 import com.dvf.project.S_Sync.domain.model.Task;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TaskService extends BasicService<Task>{
 
     public TaskService(JpaRepository<Task, Integer> repository) {
         super(repository);
+    }
+
+    public Task update_task_by_id(Task task, Integer id){
+        Optional<Task> optionalEntityDataBse = this.find_by_id(id);
+
+        if (optionalEntityDataBse.isPresent()) {
+            Task updateTask = optionalEntityDataBse.get();
+            task.setCompletionDate(updateTask.getCompletionDate());
+            task.setSubmitDate(updateTask.getSubmitDate());
+            BeanUtils.copyProperties(task, updateTask, "id");
+            return this.create(updateTask);
+        } else {
+            throw new IllegalArgumentException("not fould");
+        }
     }
 
 }

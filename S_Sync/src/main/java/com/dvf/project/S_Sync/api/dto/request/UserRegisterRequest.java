9b560efa5e56791f859b55543149fbda5b12f921
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-23 09:39:37 -0300

    FEAT[Request User Register]:Adesão do DTO Request para registro de Usuario

---
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/api/dto/request/UserRegisterRequest.java
@@ -0,0 +1,15 @@
+package com.dvf.project.S_Sync.api.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserRegisterRequest {
+
+
+    private String username;
+    private String password;
+
+
+}

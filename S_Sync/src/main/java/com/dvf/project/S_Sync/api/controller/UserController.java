b99c90cb6385dbea51a29a1ea7f114589fe0d047
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-23 10:13:25 -0300

    REFACTOR[EbdPoint Usuario]:Alteração de Users para users no requestMapping

--- S_Sync/src/main/java/com/dvf/project/S_Sync/api/controller/UserController.java
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/api/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Users")
+@RequestMapping("/users")
 public class UserController {
 
     private UserService userService;

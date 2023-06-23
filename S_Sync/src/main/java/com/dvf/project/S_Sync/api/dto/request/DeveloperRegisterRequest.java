975afa7aa712b363a2024ee618bd05d5058e18ed
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-23 10:11:16 -0300

    FEAT[DeveloperRegisterRequest]: Criação do DTo Register Request para o Desenvolvedor

---
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/api/dto/request/DeveloperRegisterRequest.java
@@ -0,0 +1,11 @@
+package com.dvf.project.S_Sync.api.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeveloperRegisterRequest {
+    private String name;
+    private String email;
+}

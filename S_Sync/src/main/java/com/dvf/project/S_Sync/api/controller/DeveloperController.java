29e9fcfdcbe12bf3922b573ffb80279ca450d956
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-26 09:25:24 -0300

    CHORE[EndPoint POST/developers]: Criação do endpoint que registra um novo desenvolvedor

--- S_Sync/src/main/java/com/dvf/project/S_Sync/api/controller/DeveloperController.java
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/api/controller/DeveloperController.java
@@ -1,14 +1,11 @@
 package com.dvf.project.S_Sync.api.controller;
 
 import com.dvf.project.S_Sync.domain.model.Developer;
-import com.dvf.project.S_Sync.domain.model.Task;
 import com.dvf.project.S_Sync.domain.services.DeveloperService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +29,12 @@ public class DeveloperController {
             return new ResponseEntity<>(developerList, HttpStatus.OK);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Developer> create_task(@RequestBody Developer developer) {
+        developerService.create(developer);
+        return new ResponseEntity<>(developer, HttpStatus.CREATED);
     }
 
 

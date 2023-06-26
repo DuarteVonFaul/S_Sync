02377ceaa5cf7875de6e20be17d3ced4fc0b9345
DuarteVonFaul <duarteriograndense@gmail.com>
2023-06-26 09:15:10 -0300

    Initialize[]:criação do Controler do Desenvolvedor

---
+++ S_Sync/src/main/java/com/dvf/project/S_Sync/api/controller/DeveloperController.java
@@ -0,0 +1,9 @@
+package com.dvf.project.S_Sync.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/developers")
+public class DeveloperController {
+}
